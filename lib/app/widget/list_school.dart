eacbc4e089ebd60b7cdaf2b4bd39b90e9c8dfee0
whymaull <first.wahyumaul21@gmail.com>
2024-01-14 01:20:41 +0700

    Add Card

---
+++ lib/app/widget/list_school.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class ListSchool extends StatelessWidget {
+  final String title;
+  final String imagePath;
+
+  const ListSchool({Key? key, required this.title, required this.imagePath})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(40),
+        side: const BorderSide(
+          color: Colors.black,
+          width: 1.5,
+        ),
+      ),
+      child: ListTile(
+        contentPadding: const EdgeInsets.all(0),
+        title: Text(
+          title,
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
+        leading: ClipRRect(
+          borderRadius: BorderRadius.circular(38),
+          child: SizedBox(
+            width: 80,
+            height: 180,
+            child: Image.asset(
+              imagePath,
+              width: 90,
+              height: 90,
+              fit: BoxFit.fill,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
