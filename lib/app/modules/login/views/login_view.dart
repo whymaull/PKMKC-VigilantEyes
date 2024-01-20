4ae72ef9a322e2aa82035a7376dd2f2d05de7d58
whymaull <first.wahyumaul21@gmail.com>
2024-01-20 15:29:17 +0700

    Add Login Page

---
+++ lib/app/modules/login/views/login_view.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../controllers/login_controller.dart';
+
+class LoginView extends GetView<LoginController> {
+  const LoginView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          margin: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Image.asset('assets/login.png'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
